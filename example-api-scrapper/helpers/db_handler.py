0ca0f15b5984b063dff758eb7a26cef1244fbf2c
Spenser White <swhite@henselphelps.com>
2020-12-30 17:06:11 -0700

    WIP update docker-compose

--- example-api-scrapper/helpers/db_handler.py
+++ example-api-scrapper/helpers/db_handler.py
@@ -1,13 +1,17 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.base import Engine as SQLAlchemyEngine
+from sqlalchemy.orm.session import Session as SQLAlchemySession
 
 
 class DbHandler:
     def __init__(self) -> None:
         self.engine = self.__get_engine()
+        self.session = self.__get_session()
 
-    def __get_engine(self):
-        thing = create_engine(
+    def __get_engine(self) -> SQLAlchemyEngine:
+        return create_engine(
             f"postgresql://\
             {os.environ['DB_USERNAME']}:\
             {os.environ['DB_PASSWORD']}@\
@@ -15,4 +19,7 @@ class DbHandler:
             {os.environ['DB_PORT']}/\
             {os.environ['DB_DATABASE']}"
         )
-        print(type(thing))
+
+    def __get_session(self) -> SQLAlchemySession:
+        Session = sessionmaker(bind=self.engine)
+        return Session()
