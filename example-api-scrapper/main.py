eab0ef234e68ac09d7f425445be0b3a3ac6ee451
Spenser White <swhite@henselphelps.com>
2021-01-22 18:55:43 -0700

    Add argparse

--- example-api-scrapper/main.py
+++ example-api-scrapper/main.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import argparse
+from typing import Dict
 from helpers.api_handler import ApiHandler
 from helpers.db_handler import DbHandler
 
@@ -11,8 +13,14 @@ def main(testing: bool):
         db_handler.handle_new_releases(new_releases)
 
 
+def __get_args() -> Dict[str, bool]:
+    parser = argparse.ArgumentParser("main.py")
+    parser.add_argument("-t", "--testing", action="store_true", default=False)
+    return vars(parser.parse_args())
+
+
 if __name__ == "__main__":
     logging_level = os.getenv("LOGGING_LEVEL", "INFO").upper()
-    testing = os.getenv("TESTING", False)
     logging.basicConfig(level=logging_level)
-    main(testing)
+    args = __get_args()
+    main(args["testing"])
